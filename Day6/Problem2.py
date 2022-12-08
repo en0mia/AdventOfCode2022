21100a4f76b3574e5b5a3a465c62017b64a86e79
Simone Nicol <en0mia.dev@gmail.com>
2022-12-08 22:41:27 +0100

    :sparkles: Add day 6 solutions.

---
+++ Day6/Problem2.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def main():
+    filename = sys.argv[1]
+    f = open(filename, 'r')
+    input_text = f.read().strip()
+    f.close()
+
+    i = 0
+    while i < len(input_text):
+        found = set()
+        j = 0
+        solution = -1
+        while j < 14:
+            if input_text[i + j] in found:
+                break
+            found.add(input_text[i + j])
+            if len(found) == 14:
+                solution = i + j + 1
+                break
+            j += 1
+        if solution != -1:
+            print(f'solution: %d' % solution)
+            exit()
+        i += 1
+
+
+if __name__ == '__main__':
+    main()
